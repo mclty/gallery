2931458bd1c8978cf67e8486091ec97650d8e3d7
mclty <liutianyu64@yahoo.com>
2015-09-23 13:55:32 +0800

    description

--- script/showPic.js
+++ script/showPic.js
@@ -2,4 +2,8 @@ function showPic(whichpic){
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
+	
+	var description = document.getElementById("description");
+	var text = whichpic.getAttribute("tittle");
+	description.firstChild.nodeValue = text;
 }
